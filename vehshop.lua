dd14217b0b1458fea3775738e02db3eaffb625bf
Musat2004 <81585268+Musat2004@users.noreply.github.com>
2021-03-29 21:53:02 +0300

    Update vehshop.lua

--- vehshop.lua
+++ vehshop.lua
@@ -64,6 +64,368 @@ local vehshopDefault = {
 				
 			}
 		},
+                ["acura"] = {
+			title = "Acura",
+			name = "acura",
+			buttons = {
+				{name = "Acura TL Type-S", costs = 20000, description = {}, model = "tltypes"},
+			}
+		},
+		["astonmartin"] = {
+			title = "Aston Martin",
+			name = "astonmartin",
+			buttons = {
+				{name = "Aston Martin DBX Carbon Edition", costs = 60000, description = {}, model = "amdbx"},
+				{name = "2013 Aston Martin Vanquish", costs = 75000, description = {}, model = "ast"},
+			}
+		},
+		["audi"] = {
+			title = "Audi",
+			name = "audi",
+			buttons = {
+				{name = "1995 Audi Cabriolet (RS2) (B4)", costs = 45000, description = {}, model = "80B4"},
+				{name = "1983 Audi Quattro Sport", costs = 25000, description = {}, model = "audquattros"},
+				{name = "2017 Audi A4 Quattro ABT", costs = 20000, description = {}, model = "aaq4"},
+				{name = "2020 Audi Q8 50TDI", costs = 38000, description = {}, model = "q820"},
+				{name = "2013 Audi R8 V10", costs = 90000, description = {}, model = "r8ppi"},
+				{name = "2020 Audi R8", costs = 90000, description = {}, model = "r820"},
+				{name = "2016 Audi RS6 C7", costs = 30000, description = {}, model = "rs6"},
+				{name = "2020 Audi RS7", costs = 140000, description = {}, model = "rs72020"},
+				{name = "1998 Audi S8 D2/PFL", costs = 18000, description = {}, model = "s8d2"},
+				{name = "2016 Audi SQ7", costs = 60000, description = {}, model = "sq72016"},
+				{name = "2010 Audi TT RS", costs = 65000, description = {}, model = "ttrs"},
+				{name = "Audi RS5", costs = 165000, description = {}, model = "rs5"},
+			}
+		},
+
+		["bentley"] = {
+			title = "Bentley",
+			name = "bentley",
+			buttons = {
+				{name = "Bentley Bentayga", costs = 100000, description = {}, model = "bbentayga"},
+			}
+		},
+		["bmw"] = {
+			title = "BMW",
+			name = "bmw",
+			buttons = {
+				{name = "2004 BMW 760Li Individual (E66/PFL)", costs = 70000, description = {}, model = "cgts"},
+				{name = "1995 BMW M5 E34", costs = 20000, description = {}, model = "760li04"},
+				{name = "2016 BMW M2", costs = 24000, description = {}, model = "e34"},
+				{name = "1997 BMW M3 E36", costs = 150000, description = {}, model = "m2"},
+				{name = "2008 BMW M3 e92", costs = 27000, description = {}, model = "m3e36"},
+				{name = "2015 BMW M3 (F80)", costs = 60000, description = {}, model = "m3e92"},
+				{name = "2015 BMW M4 F82", costs = 80000, description = {}, model = "m3f80"},
+				{name = "2015 BMW M4 F82", costs = 75000, description = {}, model = "m4f82"},
+				{name = "BMW M6 F13 Shadow Line", costs = 80000, description = {}, model = "m6f13"},
+				{name = "2019 BMW Z4 M40i", costs = 58000, description = {}, model = "z419"},
+			}
+		},
+		["bugatti"] = {
+			title = "Bugatti",
+			name = "bugatii",
+			buttons = {
+				{name = "2020 Bugatti Bolide", costs = 220000, description = {}, model = "bolide"},
+			}
+		},
+		["cadillac"] = {
+			title = "Cadillac",
+			name = "cadillac",
+			buttons = {
+				{name = "2016 Cadillac ATS-V Coupe", costs = 96000, description = {}, model = "cats"},
+				{name = "2021 Cadillac Escalade", costs = 60000, description = {}, model = "cesc21"},
+			}
+		},
+				["chevrolet"] = {
+			title = "Chevrolet",
+			name = "chevrolet",
+			buttons = {
+				{name = "2009 Chevrolet Tahoe", costs = 88000, description = {}, model = "09tahoe"},
+				{name = "2015 Chevrolet Tahoe", costs = 88000, description = {}, model = "15tahoe"},
+				{name = "2020 Chevrolet Camaro SS", costs = 85000, description = {}, model = "2020ss"},
+				{name = "2017 Chevrolet Camaro RS", costs = 65000, description = {}, model = "camrs17"},
+				{name = "2021 Chevrolet Tahoe RST", costs = 75000, description = {}, model = "tahoe21"},
+				{name = "Chevrolet Corvette C5 Z06", costs = 100000, description = {}, model = "corvettec5z06"},
+				{name = "2009 Chevrolet Corvette ZR1", costs = 120000, description = {}, model = "czr1"},
+				{name = "2014 Chevrolet Corvette C7 Stingray", costs = 130000, description = {}, model = "c7"},
+				{name = "2020 Chevrolet Corvette C8", costs = 145000, description = {}, model = "c8"},
+			}
+		},
+				["doge"] = {
+			title = "Doge",
+			name = "doge",
+			buttons = {
+				{name = "2016 Dodge Charger", costs = 130000, description = {}, model = "16charger"},
+				{name = "1999 Dodge Viper GTS ACR", costs = 100000, description = {}, model = "99viper"},
+				{name = "Dodge Charger Hellcat Widebody 2021", costs = 200000, description = {}, model = "chr20"},
+				{name = "2015 Dodge RAM 2500", costs = 40000, description = {}, model = "ram2500"},
+				{name = "2017 Dodge RAM 1500 Rebel TRX", costs = 70000, description = {}, model = "trx"},
+			}
+		},
+				["ferrari"] = {
+			title = "Ferrari",
+			name = "ferarri",
+			buttons = {
+				{name = "Ferrari 488 Spider", costs = 120000, description = {}, model = "488"},
+				{name = "Ferrari F430 Scuderia", costs = 260000, description = {}, model = "f430s"},
+				{name = "2018 Ferrari 812 Superfast", costs = 150000, description = {}, model = "f812"},
+				{name = "2015 Ferrari California T", costs = 160000, description = {}, model = "fct"},
+				{name = "Ferrari FXX-K Hybrid Hypercar", costs = 270000, description = {}, model = "fxxk"},
+				{name = "2015 Ferrari LaFerrari", costs = 200000, description = {}, model = "laferrari"},
+				{name = "Ferrari Enzo", costs = 150000, description = {}, model = "mig"},
+				{name = "458 Italia", costs = 150000, description = {}, model = "yFe458i1"},
+				{name = "458 Speciale", costs = 150000, description = {}, model = "yFe458i2"},
+				{name = "458 Spider (Roof Extra)", costs = 150000, description = {}, model = "yFe458s1X"},
+				{name = "458 Spider (Roofworks)", costs = 155000, description = {}, model = "yFe458s1"},
+				{name = "458 Speciale Aperta (Roof Extra)", costs = 150000, description = {}, model = "yFe458s2X"},
+				{name = "458 Speciale Aperta (Roofworks)", costs = 155000, description = {}, model = "yFe458s2"},
+				{name = "Ce se vede", costs = 250000, description = {}, model = "f8spider"},
+				--{name = "Ferrari F60 America", costs = , description = {}, model = "yFeF12A"},
+				--{name = "Ferrari F12 TRS Roadster", costs = , description = {}, model = "yFeF12T"},
+			}
+		},
+				["ford"] = {
+			title = "Ford",
+			name = "ford",
+			buttons = {
+				{name = "1978 Ford F150 XLT Ranger", costs = 15000, description = {}, model = "f15078"},
+				{name = "2012 Ford F150 SVT Raptor R", costs = 20000, description = {}, model = "f150"},
+				{name = "2005 Ford GT", costs = 170000, description = {}, model = "fgt"},
+				{name = "2017 Ford GT", costs = 260000, description = {}, model = "gt17"},
+				{name = "2015 Ford Mustang GT 50 Years Special Edition", costs = 130000, description = {}, model = "mustang50th"},
+				{name = "2021 Ford Bronco Wildtrak", costs = 100000, description = {}, model = "wildtrak"},
+				--{name = "2017 Ford Raptor", costs = , description = {}, model = "raptor2017"},
+			}
+		},
+				["honda"] = {
+			title = "Honda",
+			name = "honda",
+			buttons = {
+				{name = "Honda CRX SiR 1991", costs = 25000, description = {}, model = "honcrx91"},
+				--{name = "1992 Honda NSX-R (NA1)", costs = , description = {}, model = "na1"},
+				{name = "Honda S2000 AP2", costs = 75000, description = {}, model = "ap2"},
+				{name = "1997 Honda Civic Sedan Drag Version", costs = 500000, description = {}, model = "dragekcivick"},
+				--{name = "2018 Honda Civic Type-R (FK8)", costs = , description = {}, model = "fk8"},
+			}
+		},
+				["italdesignzerouno"] = {
+			title = "Italdesign Zerouno",
+			name = "italdesignzerouno",
+			buttons = {
+				{name = "2017 Italdesign Zerouno", costs = 170000, description = {}, model = "it18"},
+			}
+		},
+		
+				["jaguar"] = {
+			title = "Jaguar",
+			name = "jaguar",
+			buttons = {
+				{name = "Jaguar F-pace 2017 Hamann Edition", costs = 50000, description = {}, model = "fpacehm"},
+			}
+		},
+				["jeep"] = {
+			title = "Jeep",
+			name = "jeep",
+			buttons = {
+				{name = "2012 Jeep Wrangler", costs = 35000, description = {}, model = "jeep2012"},
+				{name = "Jeep Renegade", costs = 40000, description = {}, model = "jeepreneg"},
+				{name = "2015 Jeep SRT-8", costs = 950000, description = {}, model = "srt8"},
+				{name = "2018 Jeep Grand Cherokee Trackhawk Series IV", costs = 100000, description = {}, model = "trhawk"},
+			}
+		},
+				["koenigsegg"] = {
+			title = "Koenigsegg",
+			name = "koenigsegg",
+			buttons = {
+				{name = "2017 Koenigsegg Agera RS", costs = 170000, description = {}, model = "agerars"},
+				{name = "Koenigsegg Regera", costs = 175000, description = {}, model = "agerars"},
+			}
+		},
+				["lamborghini"] = {
+			title = "Lamborghini",
+			name = "lamborghini",
+			buttons = {
+				{name = "Lamborghini Huracan Super Trofeo", costs = 200000, description = {}, model = "huracanst"},
+				{name = "Lamborghini Sesto Elemento", costs = 200000, description = {}, model = "lambose"},
+				{name = "2013 Lamborghini Aventador LP700-4 Roadster", costs = 250000, description = {}, model = "lp700r"},
+				{name = "Lamborghini Aventador SVJ", costs = 300000, description = {}, model = "svj63"},
+				{name = "Lamborghini Urus", costs = 100000, description = {}, model = "urus"},
+				{name = "2013 Lamborghini Veneno LP750-4", costs = 300000, description = {}, model = "veneno"},
+			}
+		},
+				["lexus"] = {
+			title = "Lexus",
+			name = "lexus",
+			buttons = {
+				{name = "Lexus GS 350", costs = 30000, description = {}, model = "gs350"},
+				{name = "2014 Lexus IS 350", costs = 30000, description = {}, model = "is350mod"},
+				{name = "2015 Lexus RCF", costs = 40000, description = {}, model = "rcf"},
+			}
+		},
+				["landrover"] = {
+			title = "Land Rover",
+			name = "landrover",
+			buttons = {
+				{name = "1973 Land Rover Range Rover", costs = 98000, description = {}, model = "lrrr"},
+			}
+		},
+				["lotus"] = {
+			title = "lotus",
+			name = "Lotus",
+			buttons = {
+				{name = "2002 Lotus Esprit V8", costs = 170000, description = {}, model = "esprit02"},
+			}
+		},
+				["quartz"] = {
+			title = "Quartz",
+			name = "quartz",
+			buttons = {
+				{name = "Quartz Regalia 723", costs = 120000, description = {}, model = "regalia"},
+			}
+		},
+				["maserati"] = {
+			title = "Maserati",
+			name = "maserati",
+			buttons = {
+				{name = "Maserati Levante", costs = 30000, description = {}, model = "levante"},
+			}
+		},
+				["mazda"] = {
+			title = "mazda",
+			name = "Mazda",
+			buttons = {
+				{name = "1984 Mazda RX-7 Stanced Version", costs = 25000, description = {}, model = "84rx7k"},
+				{name = "2002 Mazda RX-7 FD Drag", costs = 97000, description = {}, model = "dragfd"},
+				{name = "Mazda RX7 FC3S", costs = 18000, description = {}, model = "fc3s"},
+				{name = "Mazda RX-7 FD", costs = 30000, description = {}, model = "majfd"},
+				{name = "1989 Mazda Miata NA", costs = 45000, description = {}, model = "miata3"},
+				{name = "Mazda MX-5 Miata (NA6C)", costs = 22000, description = {}, model = "na6"},
+			}
+		},
+				["mclaren"] = {
+			title = "McLaren",
+			name = "mclaren",
+			buttons = {
+				{name = "2020 McLaren Speedtail", costs = 170000, description = {}, model = "mcst"},
+			}
+		},
+				["mercedes"] = {
+			title = "Mercedes",
+			name = "mercedes",
+			buttons = {
+				{name = "2020 Mercedes-Benz AMG GT-R Roadster", costs = 55000, description = {}, model = "amggtrr20"},
+				{name = "2020 Mercedes-AMG C63s", costs = 80000, description = {}, model = "c6320"},
+				{name = "2013 Mercedes-Benz G65 AMG", costs = 140000, description = {}, model = "G65"},
+				{name = "2019 Mercedes-Benz E400 Coupe 4matic (C238)", costs = 97000, description = {}, model = "e400"},
+				{name = "Mercedes-Benz GL63 AMG", costs = 40000, description = {}, model = "gl63"},
+				{name = "2012 Mercedes-Benz C63 AMG Coupe Black Series", costs = 100000, description = {}, model = "mbc63"},
+				{name = "2014 Mercedes-Benz S500 W222", costs = 30000, description = {}, model = "s500w222"},
+				{name = "1995 Mercedes-Benz SL500", costs = 30000, description = {}, model = "sl500"},
+				{name = "Mercedes-Benz V-class 250 Bluetec LWB", costs = 80000, description = {}, model = "v250"},
+			}
+		},
+				["mitsubisi"] = {
+			title = "Mitsubisi",
+			name = "mitsubisi",
+			buttons = {
+				{name = "Mitsubishi Lancer Evo VI T.M.E (CP9A)", costs = 35000, description = {}, model = "cp9a"},
+				{name = "Mitsubishi FTO GP Version-R", costs = 22000, description = {}, model = "fto"},
+			}
+		},
+				["nissan"] = {
+			title = "Nissan",
+			name = "nisan",
+			buttons = {
+				{name = "Nissan 180SX Type-X", costs = 28000, description = {}, model = "180sx"},
+				{name = "2017 Nissan GTR", costs = 35000, description = {}, model = "gtr"},
+				{name = "2017 R35 Nissan GTR Convertible", costs = 110000, description = {}, model = "gtrc"},
+				{name = "Nissan Fairlady Z Z33", costs = 65000, description = {}, model = "maj350"},
+				{name = "Nissan Silvia S15 Spec-R", costs = 30000, description = {}, model = "nis15"},
+				{name = "2017 Nissan Titan Warrior", costs = 170000, description = {}, model = "nissantitan17"},
+				{name = "Nissan 350z Stardast", costs = 40000, description = {}, model = "ns350"},
+				{name = "Nissan 370z Pandem", costs = 45000, description = {}, model = "nzp"},
+				{name = "1998 Nissan Silvia K", costs = 18000, description = {}, model = "s14"},
+				{name = "1997 Nissan Patrol Super Safari Y60", costs = 100000, description = {}, model = "Safari97"},
+				{name = "Nissan Skyline GT-R (BNR34)", costs = 30000, description = {}, model = "skyline"},
+				{name = "Nissan 300ZX Z32", costs = 50000, description = {}, model = "z32"},
+			}
+		},
+				["porshe"] = {
+			title = "Porshe",
+			name = "porshe",
+			buttons = {
+				{name = "1978 Porsche 935 Moby Dick", costs = 300000, description = {}, model = "maj935"},
+				{name = "2018 Porsche Cayenne S", costs = 65000, description = {}, model = "pcs18"},
+				{name = "Porsche 718 Cayman S", costs = 125000, description = {}, model = "718caymans"},
+				{name = "2003 Porsche Carrera GT (980)", costs = 100000, description = {}, model = "cgt"},
+				{name = "2019 Porsche Macan Turbo", costs = 45000, description = {}, model = "pm19"},
+				{name = "2020 Porsche Taycan Turbo S", costs = 130000, description = {}, model = "taycan"},
+				{name = "Panamera", costs = 65000, description = {}, model = "panamera17turbo"},
+				{name = "Porsche", costs = 35000, description = {}, model = "pruf"},
+			}
+		},
+				["rangerover"] = {
+			title = "Range Rover",
+			name = "rangerover",
+			buttons = {
+				{name = "Range Rover Evoque", costs = 30000, description = {}, model = "rrevoque"},
+				{name = "Range Rover Vogue Startech", costs = 35000, description = {}, model = "rrst"},
+				{name = "2016 Range Rover Sport SVR", costs = 40000, description = {}, model = "rsvr16"},
+			}
+		},
+				["rollsroyce"] = {
+			title = "Rolls-Royce",
+			name = "rollsroyce",
+			buttons = {
+				{name = "2016 Rolls-Royce Dawn Onyx", costs = 100000, description = {}, model = "dawnonyx"},
+				{name = "Rolls-Royce Wraith", costs = 160000, description = {}, model = "wraith"},
+				{name = "Rolls Royce Cullinan", costs = 150000, description = {}, model = "rculi"},
+				{name = "2018 Rolls-Royce Phantom VIII", costs = 75000, description = {}, model = "rrphantom"},
+			}
+		},
+				["subaru"] = {
+			title = "Subaru",
+			name = "subaru",
+			buttons = {
+				{name = "2008 Subaru WRX STi", costs = 45000, description = {}, model = "subisti08"},
+				{name = "2004 Subaru Impreza WRX STI", costs = 50000, description = {}, model = "subwrx"},
+				{name = "1996 Subaru Alcyone SVX", costs = 28000, description = {}, model = "svx"},
+			}
+		},
+				["toyota"] = {
+			title = "Toyota",
+			name = "toyota",
+			buttons = {
+				{name = "2018 Toyota Camry XSE", costs = 20000, description = {}, model = "cam8tun"},
+				{name = "2016 Toyota Land Cruiser VXR", costs = 28000, description = {}, model = "vxr"},
+				{name = "1998 Toyota Supra Turbo (A80)", costs = 50000, description = {}, model = "toysupmk4"},
+				{name = "Toyota Mark II JZX100", costs = 22000, description = {}, model = "mk2100"},
+			}
+		},
+				["volkswagen"] = {
+			title = "Volkswagen",
+			name = "volkswagen",
+			buttons = {
+				{name = "2015 Volkswagen Golf GTI MK7", costs = 30000, description = {}, model = "golfgti7"},
+			}
+		},
+						["wmotors"] = {
+			title = "W-Motors",
+			name = "wmotors",
+			buttons = {
+				{name = "W-Motors Fenyr Supersport", costs = 200000, description = {}, model = "wmfenyr"},
+				{name = "W-Motors Lykan HyperSport", costs = 100000, description = {}, model = "lykan"},
+			}
+		},
+						["motoare"] = {
+			title = "Motoare",
+			name = "motoare",
+			buttons = {
+				{name = "Yamaha R1", costs = 20000, description = {}, model = "r1"},
+				{name = "Motor", costs = 17000, description = {}, model = "tmaxDX"},
+			}
+		},
+						
 		["jobvehicles"] = {
 			title = "job vehicles",
 			name = "job vehicles",
